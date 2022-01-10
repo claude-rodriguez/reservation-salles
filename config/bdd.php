d19ba67b5f521088c36a7c1e99fd46bf560a08cb
Alex-Zicaro <alex.zicaro@laplateforme.io>
2022-01-10 18:24:44 +0100

    bdd modif

--- config/bdd.php
+++ config/bdd.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
-$bdd = new PDO('mysql:host=localhost;dbname=reservationsalles', 'root', '');
-// $bdd = new PDO('mysql:host=localhost;dbname=claude-rodriguez_reservationsalles', 'claude', 'rodriguez');
-$bdd ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_WARNING);
-?>
+
+function connexion(){
+    
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=reservationsalles', 'root', '');
+        $bdd ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_EXCEPTION);
+        return $bdd;
+        } 
+    catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
+}
+$bdd = connexion();
