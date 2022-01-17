394e61d0019a901a9feabc9c986070d38fbdef23
Alex-Zicaro <alex.zicaro@laplateforme.io>
2022-01-17 11:40:59 +0100

    push pour marke le bg master de la night tard sah

--- planning.php
+++ planning.php
@@ -20,7 +20,7 @@ $reservations = $prep->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/reservationsalles.css">
+    <link rel="stylesheet" href="css/planning.css">
     <title>RESERVATION SALLES</title>
 </head>
 
