e44befe088a19af2f0ce251680f814b0dbcfcc56
rodriguez <claude.rodriguez@laplateforme.io>
2022-01-21 15:27:32 +0100

    plesk claude

--- config/bdd.php
+++ config/bdd.php
@@ -4,7 +4,7 @@ session_start();
 function connexion(){
     
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=reservationsalles', 'root', '');
+        $bdd = new PDO('mysql:host=localhost;dbname=claude-rodriguez_reservationsalles', 'claude13', 'claude13');
         $bdd ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_EXCEPTION);
         return $bdd;
         } 
